71334e3b4aa78426615bd2b8b165c27c42a94fd7
hikanashi <hikanasi@gmail.com>
2023-01-28 17:08:43 +0900

    Moved TestHttpClient so that it can be used in other projects. Added put and getfile support.

--- src/main/java/com/jamasoftware/services/restclient/httpconnection/TestHttpClient.java
+++ src/main/java/com/jamasoftware/services/restclient/httpconnection/TestHttpClient.java
@@ -1,6 +1,9 @@
 package com.jamasoftware.services.restclient.httpconnection;
 
 import com.jamasoftware.services.restclient.exception.RestClientException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -8,8 +11,10 @@ import org.json.simple.parser.ParseException;
 import org.skyscreamer.jsonassert.JSONAssert;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class TestHttpClient implements HttpClient {
     private static final Logger logger = LogManager.getLogger(TestHttpClient.class);
@@ -17,13 +22,24 @@ public class TestHttpClient implements HttpClient {
     private JSONObject responses;
     private JSONObject payloads;
     public TestHttpClient() {
+        loadTestData(
+            "testData" + File.separator + "testApiResponses.json", 
+            "testData" + File.separator + "testApiPayloads.json");
+    }
+
+    public TestHttpClient(String responseFile, String PayloadFile) {
+        loadTestData(responseFile, PayloadFile);
+    }
+
+    private void loadTestData(String responseFile, String PayloadFile) {
         JSONParser parser = new JSONParser();
         try {
-            responses = (JSONObject)parser.parse(new FileReader("testData" + File.separator + "testApiResponses.json"));
-            payloads = (JSONObject)parser.parse(new FileReader("testData" + File.separator + "testApiPayloads.json"));
+            responses = (JSONObject)parser.parse(new FileReader(responseFile));
+            payloads = (JSONObject)parser.parse(new FileReader(PayloadFile));
         } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
+            logger.error(e);
+        }   
+
     }
 
     @Override
@@ -65,16 +81,42 @@ public class TestHttpClient implements HttpClient {
 
     @Override
     public Response put(String url, String username, String password, String apiKey, String payload) throws RestClientException {
-        return null;
+        String key = "PUT " + url;
+        JSONObject payloadObject = (JSONObject)payloads.get(key);
+
+        String expectedPayload = ((JSONObject)payloadObject.get("payload")).toJSONString();
+        String response = ((JSONObject)payloadObject.get("response")).toJSONString();
+        Integer statusCode = (int)(long)payloadObject.get("statusCode");
+
+        if(expectedPayload == null) {
+            throw new RestClientException("Payloads object returned null for POST " + url);
+        }
+        try {
+            JSONAssert.assertEquals(expectedPayload, payload, true);
+        } catch (JSONException e) {
+            throw new RestClientException(e);
+        }
+
+        return new Response(statusCode, response);
     }
 
     @Override
     public Response putFile(String url, String username, String password, String apiKey, File file) throws RestClientException {
-        return null;
+        throw new RestClientException("PUT file upload is not supported " + url);
     }
 
     @Override
     public FileResponse getFile(String url, String username, String password, String apiKey) throws RestClientException {
-        return null;
+        String key = "GET " + url;
+        JSONObject responseObject = (JSONObject)responses.get(key);
+        String responsepath = ((JSONObject)responseObject.get("filepath")).toJSONString();
+        Integer statusCode = (int)(long)responseObject.get("statusCode");
+
+        try {
+            InputStream inputStream = new FileInputStream(responsepath);
+            return new FileResponse(statusCode, inputStream );
+        } catch (IOException e) {
+            throw new RestClientException(e);
+        }
     }
 }
